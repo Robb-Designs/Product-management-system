fa3738b7f9da4eba7978306aefb3f9fd5623f53c
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-08 13:14:44 -0400

    update: changed the function to return the dollar amount the product is discounted by

--- src/utils/discountCalculator.ts
+++ src/utils/discountCalculator.ts
@@ -5,5 +5,5 @@
 export const calculateDiscount = (price: number, discountPercentage: number): number => {
     const discountPrice = price * (discountPercentage / 100); //the api data has discount percentage as a dedcimal, so I divided it by 100 to get the actual percentage value
     console.log(`Price With Discount: (${price - discountPrice})`);
-    return discountPrice;
+    return price - discountPrice; //return the dollar amount that a product is discounted by
 }
