50d751e769f6d330b72a75a8c09a000ba4875ede
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-08 13:12:43 -0400

    created a function that returns discount price

---
+++ src/utils/discountCalculator.ts
@@ -0,0 +1,9 @@
+
+//Discount Calculator Module --------------------------------------------------------------------------------------------------------------------------
+// This module will be responsible for calculating the discount price for a product based on its original price and discount percentage.
+
+export const calculateDiscount = (price: number, discountPercentage: number): number => {
+    const discountPrice = price * (discountPercentage / 100); //the api data has discount percentage as a dedcimal, so I divided it by 100 to get the actual percentage value
+    console.log(`Price With Discount: (${price - discountPrice})`);
+    return discountPrice;
+}
