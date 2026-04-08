68f629857aa809dad6de0663a08580e5aad4e2bf
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-08 18:53:08 -0400

    update fetch logic, and mapped products from search into a list of instances, updated error handling

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,13 +1,19 @@
+// IMPORTS --------------------------------------------------------------------------------------------------------------
+import { Product } from "../models/Product.js";
+
+
 // API Request --------------------------------------------------------------------------------------------------------------
 // functions to fetch product data from dummyjson.com
 
-async function fetchProducts(product: string): Promise<Product[]> {
+export async function fetchProducts(product: string): Promise<Product[]> {
     try {
         const response = await fetch(`https://dummyjson.com/products/search?q=${product}`); //fetching product data from api
         const data = await response.json(); // using .json() to turn the data object
 
         // looping through the products array and for each item we are creating a new instance of the Product.
         return data.products.map((item: any) => {
+            // console.log(item.title);
+            // console.log(item.price);
             return new Product(
                 item.id,
                 item.title,
@@ -17,10 +23,14 @@ async function fetchProducts(product: string): Promise<Product[]> {
             );
         });
 
-    } catch {
-        console.error("Error fetching product data", Error);
-        return []; // return an empty array if there was an error fetching
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error("Error fetching product data:", error.message);
+        } else {
+            console.error("Unknown error occurred...");
+        }
+        return [];
     }
 }
 
-//console.log(fetchProducts("laptop")); // example of fetching laptop data from api, can change the search term to test with different products
+//fetchProducts("phone"); // example of fetching laptop data from api, can change the search term to test with different products
