a98bd4832f3de1f69e5d90fbafc1ef5b44ddd8d6
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-08 16:07:01 -0400

    Update tax rate for groceries and improve tax calculation logic

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,11 +1,22 @@
 // Tax Calculator Module --------------------------------------------------------------------------------------------------------------------------
 
-export const calculateTax = (price: number): number => {
-    const taxRate = 4.75
-    const taxedPrice = price * (taxRate / 100);
+export const calculateTax = (price: number, category: string): number => {
+    let taxRate: number;
     
+    // usedd switch: basically cleaner if statement
+    //groceries are taxed at a lower rate, all others use default rate
+    switch (category) {
+        case "groceries":
+            taxRate = 3;
+            break;
+        default:
+            taxRate = 4.75;
+    }
+
+    const taxedPrice = price * (taxRate / 100);
+
     return Number(taxedPrice.toFixed(2)); // returns the tax amount, rounded to 2 decimal places
 }
 
-//console.log(calculateTax(100));
+//console.log(calculateTax(100, "groceries"));
 
